a637167f464e083ba59426c1b29a0c9065d903d7
MathiasBal <43315288+MathiasBal@users.noreply.github.com>
2020-03-13 09:30:23 +0100

    Add files via upload

---
+++ assignment01/cmake-build-debug/CMakeFiles/assignment01.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assignment01.dir/filat.c.o"
+  "CMakeFiles/assignment01.dir/lsln.c.o"
+  "CMakeFiles/assignment01.dir/main.c.o"
+  "assignment01"
+  "assignment01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/assignment01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
